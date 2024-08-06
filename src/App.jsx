957baa0ebae23d342a370608c220f5a62ce16b31
bhuvneshwari <bhuvneshwarirathore4@gmail.com>
2024-08-06 15:08:22 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import './App.css';
 import Chat from './Components/Chat';
 import Detail from './Components/Detail';
 import List from './Components/List';
+import Login from './Components/Login';
+import Notification from './Components/Notification';
 
 function App() {
+
+  const user = false
   return (
     <div className='bg-[url("https://wallpapercave.com/wp/C3sLgvn.png")] h-screen flex items-center justify-center'>
-      <div className='max-w-[90%] m-auto h-[87%] bg-gray-900 rounded-xl backdrop-blur-[8px] saturate-[180%] text-white border border-gray-800 bg-transparent flex'>
-        <List  />
-        <Chat  />
-        <Detail  />
+      <div className='w-[90%] m-auto h-[87%] bg-gray-900 rounded-xl backdrop-blur-[8px] saturate-[180%] text-white border border-gray-800 bg-transparent flex'>
+        {user ? (
+          <>
+            <List />
+            <Chat />
+            <Detail />
+          </>
+        ) : (
+          <Login />
+        )}
+        <Notification/>
       </div>
     </div>
   );
---
+++ src/Components/Login.jsx
@@ -0,0 +1,104 @@
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+
+const Login = () => {
+  const [avator, setAvator] = useState({
+    file: null,
+    url: '',
+  });
+
+  const handleAvator = (e) => {
+    setAvator({
+      file: e.target.files[0],
+      url: URL.createObjectURL(e.target.files[0]),
+    });
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault;
+    toast.warn("")
+  };
+
+  return (
+    <div className='login flex items-center gap-32 justify-center w-full'>
+      <div className='item flex flex-col items-center gap-5 justify-center'>
+        <div className='font-bold text-2xl mb-3'>
+          Welcome <span className='text-[#FFA500]'>back</span>
+        </div>
+        <form
+          className='flex flex-col items-center justify-center gap-5'
+          onSubmit={handleLogin}
+        >
+          <input
+            type='email'
+            placeholder='Email'
+            name='email'
+            className='px-5 py-3 border-none outline-none bg-[rgba(248,249,252,0.53)] rounded-md placeholder:text-gray-800 text-gray-800'
+          />
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            className='px-5 py-3 border-none outline-none bg-[rgba(248,249,252,0.53)] rounded-md placeholder:text-gray-800 text-gray-800'
+          />
+          <button className='bg-[#FFA500] bg-opacity-75 w-full py-2 rounded-md my-2 text-lg font-semibold hover:scale-105 transform cursor-pointer'>
+            Sign In
+          </button>
+        </form>
+      </div>
+
+      <div className='seperator border-r border-[#dddddd35] h-5/6 w-[2px]' />
+
+      <div className='item flex flex-col items-center gap-5 justify-center'>
+        <div className='mb-3 font-bold text-2xl'>
+          Create an <span className='text-[#FFA500]'>Account</span>
+        </div>
+        <form className='flex flex-col items-center justify-center gap-5'>
+          <label
+            htmlFor='file'
+            className='flex items-center justify-between cursor-pointer underline gap-5'
+          >
+            <img
+              src={
+                avator.url ||
+                'https://www.kindpng.com/picc/m/394-3947019_round-profile-picture-png-transparent-png.png'
+              }
+              alt='image'
+              className='size-12 object-cover rounded-xl opacity-60'
+            />
+            Upload an image
+          </label>
+          <input
+            type='file'
+            id='file'
+            style={{ display: 'none' }}
+            onChange={handleAvator}
+          />
+          <input
+            type='text'
+            placeholder='Username'
+            name='username'
+            className='px-5 py-3 border-none outline-none bg-[rgba(248,249,252,0.53)] rounded-md placeholder:text-gray-800 text-gray-800'
+          />
+          <input
+            type='email'
+            placeholder='Email'
+            name='email'
+            className='px-5 py-3 border-none outline-none bg-[rgba(248,249,252,0.53)] rounded-md placeholder:text-gray-800 text-gray-800'
+          />
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            className='px-5 py-3 border-none outline-none bg-[rgba(248,249,252,0.53)] rounded-md placeholder:text-gray-800 text-gray-800 '
+          />
+          <button className='bg-[#FFA500] bg-opacity-75 w-full py-2 rounded-md my-2 text-lg font-semibold cursor-pointer hover:scale-105 transform'>
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
